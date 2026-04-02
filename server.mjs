efe3a49262f1ac150930616071a73a59752fb555
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:50:24 -0500

    style: js-beautify formatting (server.mjs)

--- server.mjs
+++ server.mjs
@@ -1,18 +1,26 @@
-import {normalizeRequest,mapResDTO,applyResponse} from './modules/http-fetch.mjs';
-import {addCorsHeaders} from './modules/cors-headers.mjs';
+import {
+  normalizeRequest,
+  mapResDTO,
+  applyResponse
+} from './modules/http-fetch.mjs';
+import {
+  addCorsHeaders
+} from './modules/cors-headers.mjs';
 import fetch from 'node-fetch';
-import{fileFetch,fileMap} from './modules/remote-files.mjs';
+import {
+  fileFetch,
+  fileMap
+} from './modules/remote-files.mjs';
 
 let hostTarget = 'www.google.com';
 let hostList = [];
 
+export async function serverRequestResponse(reqDTO) {
 
-export async function serverRequestResponse(reqDTO){
-  
-  let resDTO={};
-  resDTO.headers={};
+  let resDTO = {};
+  resDTO.headers = {};
   let hostProxy = reqDTO.host;
-  hostTarget=hostProxy.replace('-router-servleteer.vercel.app','').replaceAll('-','.');
+  hostTarget = hostProxy.replace('-router-servleteer.vercel.app', '').replaceAll('-', '.');
   hostList.push(hostTarget);
   let path = reqDTO.shortURL.replaceAll('*', '');
   let pat = path.split('?')[0].split('#')[0];
@@ -23,7 +31,7 @@ export async function serverRequestResponse(reqDTO){
   }
   if (pat == '/link-resolver.js') {
     let response = await fileFetch(pat);
-    resDTO = mapResDTO(resDTO,response);
+    resDTO = mapResDTO(resDTO, response);
     return resDTO;
 
   }
@@ -32,60 +40,51 @@ export async function serverRequestResponse(reqDTO){
   reqDTO.headers.host = hostTarget;
   reqDTO.headers.referer = hostTarget;
 
-    /* fetch from your desired target */
-    let response = await fetch('https://' + hostTarget + path,reqDTO);
+  /* fetch from your desired target */
+  let response = await fetch('https://' + hostTarget + path, reqDTO);
 
-    /* if there is a problem try redirecting to the original */
-    if (response.status > 399) {
-      resDTO.headers['location']='https://' + hostTarget + path;
-      resDTO.statusCode = 302;
-      return resDTO;
-    }
+  /* if there is a problem try redirecting to the original */
+  if (response.status > 399) {
+    resDTO.headers['location'] = 'https://' + hostTarget + path;
+    resDTO.statusCode = 302;
+    return resDTO;
+  }
 
+  resDTO = mapResDTO(resDTO, response);
 
-    resDTO = mapResDTO(resDTO,response);
+  /* check to see if the response is not a text format */
+  let ct = response.headers.get('content-type');
 
-    
-    /* check to see if the response is not a text format */
-    let ct = response.headers.get('content-type');
+  resDTO.headers['content-type'] = ct;
 
-  
-    resDTO.headers['content-type'] = ct;
-  
+  if ((ct) && (!ct.includes('image')) && (!ct.includes('video')) && (!ct.includes('audio'))) {
 
-    
-    if ((ct) && (!ct.includes('image')) && (!ct.includes('video')) && (!ct.includes('audio'))) {
+    /* Copy over target response and return */
+    let resBody = await response.text();
+    if (ct.includes('html') || ct.includes('xml') || pat.endsWith('.html') || pat.endsWith('.xhtml')) {
+      let sty = '';
+      if (hostTarget == 'www.amazon.com') {
 
-      /* Copy over target response and return */
-      let resBody = await response.text();
-if(ct.includes('html')||ct.includes('xml')||pat.endsWith('.html')||pat.endsWith('.xhtml')){
-      let sty='';
-      if(hostTarget=='www.amazon.com'){
-        
-        sty='<style>html{filter: invert(1) hue-rotate(180deg);}</style>';
-        
-        }
-      
-      
+        sty = '<style>html{filter: invert(1) hue-rotate(180deg);}</style>';
+
+      }
+
       resBody = resBody.replace('<head>',
         `<head modified>
-        `+sty+`
-        <script src=https://`+ hostProxy + `/link-resolver.js host-list=` + btoa(JSON.stringify(hostList)) + `></script>`);
+        ` + sty + `
+        <script src=https://` + hostProxy + `/link-resolver.js host-list=` + btoa(JSON.stringify(hostList)) + `></script>`);
     }
-      resDTO['Content-Length']=new Blob([resBody]).size;
-      resDTO.body = resBody;
-      return resDTO;
+    resDTO['Content-Length'] = new Blob([resBody]).size;
+    resDTO.body = resBody;
+    return resDTO;
 
+  } else {
 
-    } else {
-      
-      let resBody = Buffer.from(await response.arrayBuffer());
-      resDTO['Content-Length']=resBody.length;
-      resDTO.body = resBody;
-      return resDTO;
+    let resBody = Buffer.from(await response.arrayBuffer());
+    resDTO['Content-Length'] = resBody.length;
+    resDTO.body = resBody;
+    return resDTO;
 
-    }
-  
-
+  }
 
 }
