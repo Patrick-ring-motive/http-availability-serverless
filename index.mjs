f7f4229750f9d8990e30fdc40a65a63bc01a3405
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:50:04 -0500

    style: js-beautify formatting (index.mjs)

--- index.mjs
+++ index.mjs
@@ -1,12 +1,21 @@
 import fetch from 'node-fetch';
 import http from 'http';
-import {addCorsHeaders} from './modules/cors-headers.mjs';
-import {normalizeRequest,mapResHeaders,applyResponse} from './modules/http-fetch.mjs';
+import {
+  addCorsHeaders
+} from './modules/cors-headers.mjs';
+import {
+  normalizeRequest,
+  mapResHeaders,
+  applyResponse
+} from './modules/http-fetch.mjs';
 import maintain from './modules/auto-maintain.mjs';
-import {availReq,availRes} from './modules/availability.mjs';
-import {serverRequestResponse} from './server.mjs';
-
-
+import {
+  availReq,
+  availRes
+} from './modules/availability.mjs';
+import {
+  serverRequestResponse
+} from './server.mjs';
 
 let server = http.createServer(availReq(onRequest));
 
@@ -14,9 +23,9 @@ server.listen(3000);
 maintain(server);
 
 async function onRequest(req, res) {
- res=availRes(res);
- let reqDTO = await normalizeRequest(req);
- let resDTO = await serverRequestResponse(reqDTO);
-  return await applyResponse(res,resDTO);
+  res = availRes(res);
+  let reqDTO = await normalizeRequest(req);
+  let resDTO = await serverRequestResponse(reqDTO);
+  return await applyResponse(res, resDTO);
 
 }
